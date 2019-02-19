7a30937c1412d1fc968ff92bfaa0ff20b01cc9fb
Evan Briolat <briolate@gmail.com>
2019-02-19 11:11:35 -0600

    Code refactoring and file restructuring

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,41 @@
+const Event = require('../../models/event');
+
+const { transformEvent } = require('./merge');
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5c6b111a80dc3011e78b02a0'
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById('5c6b111a80dc3011e78b02a0');
+
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
